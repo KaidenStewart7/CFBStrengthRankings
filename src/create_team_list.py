b4428f3c9a54396e630b2dbe0e24d81f33efec81
Kaiden Stewart <stewart.2292@osu.edu>
2025-10-09 18:45:34 -0400

    created team list

---
+++ src/create_team_list.py
@@ -0,0 +1,19 @@
+from fetch_api_data import FetchApiData
+import os
+
+# Get path to the current script
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to txt/teams_list.txt (relative to project root)
+output_path = os.path.join(base_dir, "..", "txt", "teams_list.txt")
+
+# Creates a list of teams and there conferences to be printed to the file
+def main():
+    teams_info = FetchApiData().fetch_cfb_teams()
+    for team in teams_info:
+        if team['classification'] == 'fbs':
+            with open(output_path, "a") as file:
+                file.write(f"{team['school']},{team['conference']}\n")
+
+if __name__ == "__main__":
+    main()
---
+++ src/fetch_api_data.py
@@ -0,0 +1,26 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+#Load variables from .env file
+load_dotenv()
+
+api_token = os.getenv("CFP_API_TOKEN")
+headers = {
+    "Authorization": f"Bearer {api_token}"
+}
+
+# Class to fetch data from the College Football Data API
+class FetchApiData:
+    # Fetches_cfb_teams from the College Football Data API 
+    # Returns the JSON of the response
+    def fetch_cfb_teams(self):
+        url = "https://api.collegefootballdata.com/teams"
+        try: 
+            response = requests.get(url, headers=headers)
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching data from {url}: {e}")
+
+
+
