1f8d43c1c8778ba44638e406bc1585049f18f698
Kaiden Stewart <stewart.2292@osu.edu>
2025-10-09 23:23:24 -0400

    Removed all games from database due to error

--- src/load_games.py
+++ src/load_games.py
@@ -2,7 +2,7 @@ from fetch_api_data import FetchApiData
 from database_queries import DatabaseQueries
 
 # Week to load games of
-WEEK = 3
+WEEK = 1
 
 # This Class will load games from the College Football Data API into the database
 class Load_Games:
