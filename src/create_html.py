f0ff1ef31f19981797517741f12eda8dab8d7930
Kaiden Stewart <stewart.2292@osu.edu>
2025-10-11 00:04:51 -0400

    Added css sytles

--- src/create_html.py
+++ src/create_html.py
@@ -5,7 +5,6 @@ YEAR = 2025
 
 # Current Week
 WEEK = 2
-
 # Get path to the current script
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -33,7 +32,7 @@ class CreateHTML():
                         count += 1
                     name = line[line.index(".") + 2:line.index(":")]
                     rating = line[line.index(":") + 2:len(line) - 1]
-                    self.teams.append({"Name": name, "Rating": float(rating).__round__(4)})
+                    self.teams.append({"Name": name, "Rating": '  {:.4f}'.format(float(rating).__round__(4))})
                 else:
                     count += 1
 
@@ -42,23 +41,23 @@ class CreateHTML():
             file.write("<!DOCTYPE html>\n")
             file.write("<html lang=\"en\">\n")
             file.write("    <head>\n")
-            file.write("        <link rel=\"stylesheet\" href=\"styles.css\">\n")
-            file.write("        <title>College Football Strength Ratings</title>\n")
+            file.write("        <link rel=\"stylesheet\" href=\"styles.css\" media=\"screen\" />\n")
+            file.write("        <title>College Football Strength Rankings</title>\n")
             file.write("        <meta charset=\"UTF-8\" >\n")
             file.write("    </head>\n")
             file.write("    <body>\n")
-            file.write("        <h1>College Football Strength Ratings</h1>\n")
+            file.write("        <h1>College Football Strength Rankings</h1>\n")
             file.write("        <p>\n")
             file.write("            Below you can find the weekly top 25 college football teams according to their strength rankings. These are power rankings that take into account team performance, conference strength, and the talent of the team determined by their <a href=\"https://247sports.com/season/2025-football/collegeteamtalentcomposite/\"> 247 Sports Talent Composite Ranking</a>. Each week, new rankings will be added. Early in the season, the main factor weighting the strength ratings will be their talent rating; however, as the season progresses and more games are played, each team’s rankings will factor in their \"resume rating\" more than their talent rating. Eventually, after each team plays 12 games against FBS opponents, their strength rating will be comprised only of their resume rating. A team's resume rating factors in their performance as well as the strength of their conference they are in. \n")
             file.write("        </p>\n")
             file.write("        <h2> Top 25 Rankings </h2>\n")
-            file.write("        <ul>\n")
+            file.write("        <div>\n")
             week_count = WEEK
             while week_count > 0:
                 week_path = f"week{week_count}.html"
-                file.write(f"            <li><a href=\"{week_path}\">Week {week_count} Rankings</a></li>\n")
+                file.write(f"            <p><a href=\"{week_path}\">Week {week_count} Rankings</a></p>\n")
                 week_count -= 1
-            file.write("        </ul>\n")
+            file.write("        </div>\n")
             file.write("    </body>\n")
             file.write("</html>\n")
 
@@ -70,17 +69,17 @@ class CreateHTML():
             file.write("<!DOCTYPE html>\n")
             file.write("<html lang=\"en\">\n")
             file.write("    <head>\n")
-            file.write("        <link rel=\"stylesheet\" href=\"styles.css\">\n")
-            file.write(f"        <title>College Football Strength Ratings - Week {week_count}</title>\n")
+            file.write("        <link rel=\"stylesheet\" href=\"styles.css\" media=\"screen\" />\n")
+            file.write(f"        <title>College Football Strength Rankings - Week {week_count}</title>\n")
             file.write("        <meta charset=\"UTF-8\" >\n")
             file.write("    </head>\n")
             file.write("    <body>\n")
-            file.write(f"        <h1>College Football Strength Ratings - Week {week_count}</h1>\n")
+            file.write(f"        <h1>College Football Strength Rankings - Week {week_count}</h1>\n")
             file.write("        <ol>\n")
             count = 0
             while count < 25:
                 team = self.teams[count]
-                file.write(f"            <li><span>{team['Name']}</span> <span>{team['Rating']}</span></li>\n")
+                file.write(f"            <li><span class=\"ranking\">{count + 1}.</span><span class=\"team\">{team['Name']}</span> <span class=\"rating\">{team['Rating']}</span></li>\n")
                 count += 1
             file.write("        </ol>\n")
             file.write("        <p>\n")
